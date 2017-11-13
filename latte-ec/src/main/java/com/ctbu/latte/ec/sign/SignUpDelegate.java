046921d08e0eedc3c44794473009d0917c80c11f
CaiPengFei <915207398@qq.com>
2017-11-14 00:10:59 +0800

    logo todo

---
+++ latte-ec/src/main/java/com/ctbu/latte/ec/sign/SignUpDelegate.java
@@ -0,0 +1,106 @@
+package com.ctbu.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ctbu.latte.delegates.LatteDelegate;
+import com.ctbu.latte.ec.R;
+import com.ctbu.latte.ec.R2;
+import com.ctbu.latte.net.RestClient;
+import com.ctbu.latte.net.callback.ISuccess;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by chenting on 2017/11/12.
+ */
+
+public class SignUpDelegate extends LatteDelegate {
+
+    @BindView(R2.id.edit_sign_up_name)
+    TextInputEditText mName = null;
+    @BindView(R2.id.edit_sign_up_email)
+    TextInputEditText mEmail = null;
+    @BindView(R2.id.edit_sign_up_phone)
+    TextInputEditText mPhone = null;
+    @BindView(R2.id.edit_sign_up_password)
+    TextInputEditText mPassword = null;
+    @BindView(R2.id.edit_sign_up_re_password)
+    TextInputEditText mRePasswrod = null;
+
+    @OnClick(R2.id.btn_sign_up)
+    void onClickSignUp(){
+        if (checkForm()){
+//            RestClient.builder()
+//                    .url("sign_up")
+//                    .params("","")
+//                    .success(new ISuccess() {
+//                        @Override
+//                        public void onSuccess(String response) {
+//
+//                        }
+//                    })
+//                    .build()
+//                    .post();
+            Toast.makeText(getContext(),"验证通过",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @OnClick(R2.id.tv_link_sign_in)
+    void onClickLink() {
+        getSupportDelegate().start(new SignInDelegate());
+    }
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRePasswrod.getText().toString();
+        boolean isPass = true;
+        if (name.isEmpty()) {
+            mName.setError("请输入姓名");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+        if (phone.isEmpty() || phone.length() != 11) {
+            mPhone.setError("手机号码错误");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRePasswrod.setError("密码验证错误");
+            isPass = false;
+        } else {
+            mRePasswrod.setError(null);
+        }
+        return isPass;
+    }
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        super.onBindView(savedInstanceState, rootView);
+    }
+}
